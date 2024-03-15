a7226990eea176389278013b7b5cc5d1c14e700e
Frederic Merkl <frederic@frederics-mbp.localdomain>
2024-03-15 13:44:08 +0100

    fixed the bug where after a result is displyed and user clicks a number, the number gets appended to the result. Now a new equations stats when user  presses a number after a result is displayed. When the user presses an operator, user cintinous to calculate with the last result.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let firstNumber = null;
 let secondNumber = null;
 let operator = null;
+let operationPerformed = false;
 
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".numButton, .operatorButton, .dot, .equalSign, .clearButton")
 const dot = document.querySelector(".dot").value;
+const operators = document.querySelectorAll(".operatorButton")
 
 function addEventListener(){
     for(let i = 0; i < buttons.length; i++){
@@ -19,36 +21,51 @@ addEventListener()
 function updateDisplay(value){
 
     if(!isNaN(value)){
+
         handleNumber(value)
     }
     if (value === ".") { // operator or dot click
-         addDot(value)
-    }
-    if(value != undefined && value.match(/[-+*/]/)){
         
-        operator = value;
+        if(operationPerformed){
+            operationPerformed = false;
+            firstNumber = null,
+            updateDisplay("0")
+            updateDisplay(".");
+            
+        } else {
+         addDot(value)
+        }
     }
-    if(value === "=" && firstNumber && secondNumber && operator){
+    if(value.match(/[-+*/]/)){
+        removeGlow()
+        if(firstNumber && secondNumber && operator){ // handles operator click when firstNumber, secondNumber and operator are privided. 
+            handleEqual()                            // operator becomes equal
+            operator = value;
+            addGlow(value)
+        }
+        else{
+            operator = value
+            addGlow(value)
+        }
+    };
+
+    if(value === "=" && firstNumber && secondNumber && operator){ // only allows the calculation to be done if all arguments are provided.
 
         handleEqual()
-    }
- 
-}
+    };
+ };
 
 
 
-function handleEqual() {
-
-      
+function handleEqual() {   
     firstNumber = parseFloat(firstNumber);
     secondNumber = parseFloat(secondNumber);
     display.innerText = operate(firstNumber, secondNumber, operator)
     firstNumber = display.innerText;
     secondNumber = null;
     operator = null;
-        
-
-    }
+    operationPerformed = true;
+ }
 
 
 
@@ -56,28 +73,37 @@ function handleEqual() {
     
 function handleNumber(value){
 
-    
     if(firstNumber == null){   // first number click
-        console.log("2.")
+        
         firstNumber = value;
         display.innerText = value;
+       
     }
     else if(firstNumber != null && operator == null){ // add to firstnumber
-        console.log("3.")   
-        firstNumber += value;
-        display.innerText += value;
-
-
-    } else if (operator){ //second number
+        console.log("second if in handle number")
         
+        if(operationPerformed){ // starts a new calc, number clicked becomes new firstNumber value. only runs when result is present.
+            console.log("inside second if in handle number")
+            firstNumber = value;
+            display.innerText = value;
+            operationPerformed = false;
+        } 
+        
+        else {      
+        firstNumber += value;   
+        display.innerText += value;
+        }
+    } 
+    else if (operator){ //second number clicks. runs when operator is provided.
+       removeGlow()
         if (secondNumber == null) {       
-            console.log("4.");
+            
             secondNumber = value;
             display.innerText = value;
         }
         
        else{
-            console.log("4.1")
+            
             secondNumber += value;
             display.innerText += value;
         } 
@@ -86,9 +112,6 @@ function handleNumber(value){
 
 }    
     
-
-
-
 
 function addDot(dot){
 
@@ -141,6 +164,22 @@ function divide(a, b) {
     return a / b;
 };
 
+function addGlow(value){
+
+  operators.forEach(operator => {
+    if(value == operator.innerText){
+        operator.classList.add("glow")
+    }})}
+
+function removeGlow(){
+
+    operators.forEach(operator => {
+        if(operator.classList.contains("glow")){
+            operator.classList.remove("glow")
+        }
+    })
+
+}
 
 
  // if(firstNumber == null && secondNumber == null && operator == null && !value){ // initial disyplay value
